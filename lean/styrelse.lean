3deee14cc2d0c77ac5b3eb406f5be005fa1e7937
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-05-08 12:53:25 +0200

    x

--- lean/styrelse.lean
+++ lean/styrelse.lean
@@ -1,22 +1,17 @@
 HEADER|Styrelse
 Organisationsnummer: 80 20 09 - 3038
 
-SMALL|Ordförande • |Tom Wedberg|9qOERR_pVO.jpg
-
-SMALL|Vice ordförande • |Birger Wenzel|AVSHhBilgl.jpg
-
-Kassör • Sten Arvidsson
-
-SMALL|Sekreterare • |Niclas Hedin|AaTEWiNLwf.jpg
-
-LINK|Ledamot • |Charlotte Kämpe|images/Charlotte_Kämpe.jpg
-
-LINK|Ledamot • |David Douhan|images/David_Douhan.jpg
-
-LINK|Ledamot • |Lennarth Eriksson|images/Lennarth_Eriksson.jpg
-
-SMALL|Materialförvaltare • |Hans Rånby|AJCCHfAWYx.jpg
-
-Revisor • ?
-
-Webmaster • ?
+Ordförande
+	SMALL|Tom Wedberg|9qOERR_pVO.jpg
+Vice ordförande
+	SMALL|Birger Wenzel|AVSHhBilgl.jpg
+Kassör
+	Sten Arvidsson
+Sekreterare
+	SMALL|Niclas Hedin|AaTEWiNLwf.jpg
+Ledamot
+	LINK|Charlotte Kämpe|images/Charlotte_Kämpe.jpg
+	LINK|David Douhan|images/David_Douhan.jpg
+	LINK|Lennarth Eriksson|images/Lennarth_Eriksson.jpg
+Materialförvaltare
+	SMALL|Hans Rånby|AJCCHfAWYx.jpg
