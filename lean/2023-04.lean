2acac7875ef04a6ee8950f5ff87ff29314aa3846
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-05-08 12:42:45 +0200

    x

--- lean/2023-04.lean
+++ lean/2023-04.lean
@@ -1,29 +1,29 @@
 HEADER|Inbjudningar April 2023
-WASA|Påskturneringen - GP|Inbjudan_Påskturneringen_2023.pdf
+APR 07-10
+	WASA|Påskturneringen - GP|Inbjudan_Påskturneringen_2023.pdf
 	Norrköping
-	2023APR 07-10
 	TOUR|Resultat|10380
 
-WASA|Påsklovsturneringen|Inbjudan_Påsklovsturnering_2023.pdf
+BOLD|APR 14
+	WASA|Påsklovsturneringen|Inbjudan_Påsklovsturnering_2023.pdf
 	Salongerna • Stockholm
-	BOLD|2023APR 14
 
-WASA|Kadettallsvenskans kval (2007-yngre)|kadett2023.pdf
+BOLD|APR 22-23 **
+	WASA|Kadettallsvenskans kval (2007-yngre)|kadett2023.pdf
 	Hägerstensåsens Medborgarhus
-	BOLD| ** 2023APR 22-23
 	TOUR|Resultat|11626
 
-WASA|Stockholmsmästerskapet i blixt|Inbjudan_Blixt_DM_2023.pdf
+APR 23
+	WASA|Stockholmsmästerskapet i blixt|Inbjudan_Blixt_DM_2023.pdf
 	Salongerna • Stockholm
-	2023APR 23
 	TOUR|Resultat|11499
 
-LINK|Näringslivet möter förorten|https://naringslivetmoterfororten.se/media
+APR 26
+	LINK|Näringslivet möter förorten|https://naringslivetmoterfororten.se/media
 	Cederquists advokatfirma • Stockholm
-	2023APR 26
 	TOUR|Resultat|11711
 
-WASA|Wasa JGP|Inbjudan_Wasa_JGP_2023.pdf
+BOLD|APR 29-30 **
+	WASA|Wasa JGP|Inbjudan_Wasa_JGP_2023.pdf
 	Salongerna • Stockholm
-	BOLD| ** 2023APR 29-30
 	TOUR|Resultat|11558
